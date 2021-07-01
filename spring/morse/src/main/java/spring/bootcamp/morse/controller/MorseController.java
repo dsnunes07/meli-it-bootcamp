6b04bfd475cd0c4c7959fbd6a183df3b34b2bcc8
dsnunes07 <daniel.nunes@mercadolivre.com>
2021-07-01 09:29:47 -0300

    morse with GET request params

--- spring/morse/src/main/java/spring/bootcamp/morse/controller/MorseController.java
+++ spring/morse/src/main/java/spring/bootcamp/morse/controller/MorseController.java
@@ -1,17 +1,15 @@
 package spring.bootcamp.morse.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring.bootcamp.morse.model.Morse;
 
 import java.util.Locale;
 
 @RestController
 public class MorseController {
-    @GetMapping(path = "/translate/{morse}")
-    public String translateMessage(@PathVariable String morse) {
-        Morse m = new Morse(morse);
+    @RequestMapping("/translate")
+    public String translateMessage(@RequestParam String message) {
+        Morse m = new Morse(message);
         return m.translate();
     }
 }
